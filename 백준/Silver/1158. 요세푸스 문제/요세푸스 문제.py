e50198243638f028ec5af77e3033bcc458f590f7
Jinyeong <139422554+jinddobaegi@users.noreply.github.com>
2024-02-26 18:36:16 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,18 +1,22 @@
-N, K = map(int, input().split())  # N: 사람 수, K: 제거 순서
-people = list([p for p in range(N)])  # 사람
-visited = list([0] * N)
-res = '<'
-turn = -1
+# 요세푸스 문제
+# 실버 4
 
-while people:
-    turn = (turn + K) % len(people)
-    x = people[turn]
-    # print(x)
-    people.remove(x)
-    # print(people)
-    res += f'{x + 1}, '
-    turn -= 1
+import sys
 
-res = res[:-2]
-res += '>'
-print(res)
+N, K = map(int, input().split())
+arr = [i for i in range(1, N + 1)]
+
+# 없앨 사람 담기
+answer = []
+# 사람 인덱스
+n = 0
+
+for t in range(N):
+    n += K - 1
+    if n >= len(arr):
+        # 반복문 돌고 돌아올 때를 생각해서 나머지로 값을 바꿔주기
+        n = n % len(arr)
+    # 팝해줘야함
+    answer.append(str(arr.pop(n)))
+# 출력 형식에 맞게 join써서 해주기 + 뜨ㅣ어쓰기도
+print("<", ", ".join(answer)[:], ">", sep="")
