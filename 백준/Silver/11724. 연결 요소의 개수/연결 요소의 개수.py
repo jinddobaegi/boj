a232dedc8fe6b79bff8416c512803cb2d8d9492c
Jinyeong <139422554+jinddobaegi@users.noreply.github.com>
2024-03-04 18:22:15 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 472 ms, Memory: 300140 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,44 +1,36 @@
-from sys import stdin
-from collections import deque
+# 연결 요소의 개수
+# 실버 2
+import sys
+sys.setrecursionlimit(10**5)
 
-input = stdin.readline
-
-# N: 정점 개수, M: 간선 수
 N, M = map(int, input().split())
-adj_list = list(list() for i in range(N+1))  # 0은 안 쓸 거임
-
+board = [[] for _ in range(N+1)]
 for i in range(M):
+    # 간선의 양끝점 (해당 점들을 다 board에 담아줌)
     u, v = map(int, input().split())
-    # 무향그래프
-    adj_list[u].append(v)
-    adj_list[v].append(u)
-
-# 1번 정점부터 돌기
-# 한 번 돌면, 그 다음 정점 돌 차례
-# 이미 탐색한 연결 요소들에 포함되어있는지 확인(visited)
-# 안 돼있을 때만 탐색
-# 총 묶음을 cnt로 세면 될 듯?
-
-visited = [0] * (N+1)
-que = deque()
+    board[u].append(v)
+    board[v].append(u)
 
 
-def bfs():
-    cnt = 0
-    for node in range(1, N+1):
-        # 해당 노드 방문기록 확인
-        if visited[node] == 0:
-            que.append(node)  # 담고 시작
-            visited[node] = 1
-            cnt += 1
-            while que:
-                n1 = que.popleft()
-                for n2 in adj_list[n1]:
-                    if visited[n2] == 0:
-                        que.append(n2)
-                        visited[n2] = 1
-
-    return cnt
+def dfs(board, v, visited):
+    # 돌기 시작할 때 일단 방문 체크해줌
+    visited[v] = True
+    for i in board[v]:
+        if not visited[i]:
+            dfs(board, i, visited)
 
 
-print(bfs())
+# 연결된 노드의 개수
+cnt = 0
+
+# 방문 지점 체크할 visited
+visited = [False] * (N+1)
+# 1부터 돌면서
+for j in range(1, N+1):
+    # 방문하지 않았다면 탐색 ㄱ
+    if not visited[j]:
+        dfs(board, j, visited)
+        # 탐색 끝나면 체크하나씩 해주고
+        cnt += 1
+
+print(cnt)
