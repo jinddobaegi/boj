319fd841e34a07be6e6ab9bdb0d28f916091e801
Jinyeong <139422554+jinddobaegi@users.noreply.github.com>
2024-07-14 02:57:10 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,22 +1,17 @@
-from sys import stdin
+import sys
+input = sys.stdin.readline
 
-input = stdin.readline
-T = int(input())
+def func(x):
+  if x==1:
+    return 1
+  elif x==2:
+    return 2
+  elif x==3:
+    return 4
+  else:
+    return func(x-1)+func(x-2)+func(x-3)
 
-# 순서도 중요함
-# 1: 1
-# 2: 2, 11
-# 3: 3, 21, 12, 111
-# 4 이후로 a+1, b+2, c+3의 형태로 나타내면
-# a는 k-1, b는 k-2, c는 k-3이 됨
-
-dp = [0] * 12  # 0 안 쓸 거임
-dp[1] = 1
-dp[2] = 2
-dp[3] = 4
-
-for i in range(4, 12):
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-
-for _ in range(T):
-    print(dp[int(input())])
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  print(func(n))
