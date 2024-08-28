045a8399bc22b9dda33e8451ea82109b037c58d4
Jinyeong <139422554+jinddobaegi@users.noreply.github.com>
2024-08-28 17:41:06 +0900

    [Gold IV] Title: 이모티콘, Time: 716 ms, Memory: 53772 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -12,7 +12,7 @@ input = stdin.readline
 # S개의 이모티콘 만드는 데 걸리는 최소 시간
 
 S = int(input())
-INF = int(1e9)
+# 0 = int(1e9)
 
 # bfs 써가면서
 # 특정 개수의 최솟값을 업데이트 해가면서
@@ -21,28 +21,33 @@ INF = int(1e9)
 def bfs():
     q = deque()  # 임티개수, 클립보드 개수
     q.append((1, 0))
-    counter = list([INF] * (S+2) for _ in range(S+2))  # 행: 이모지 수, 열: 클립보드 수, 값: cnt
-    counter[1][0] = 0
+    counter = list([0] * (S+2) for _ in range(S+2))  # 행: 이모지 수, 열: 클립보드 수, 값: cnt
+    # counter[1][0] = 0
     while q:
         emoji, cb = q.popleft()
         # 복사
-        if 0 < emoji <= S+1 and counter[emoji][emoji] == INF:
+        if 0 < emoji <= S+1 and counter[emoji][emoji] == 0:
             q.append((emoji, emoji))
             counter[emoji][emoji] = counter[emoji][cb] + 1
 
         # 붙여넣기
         if cb:
             x = emoji + cb  # emoji 개수에 클립보드의 c를 붙여넣기한 개수 == x
-            if 0 < x <= S+1 and counter[x][cb] == INF:
+            if 0 < x <= S+1 and counter[x][cb] == 0:
                 q.append((x, cb))
                 counter[x][cb] = counter[emoji][cb] + 1
 
         # 한 개 삭제
-        if 0 < emoji-1 and counter[emoji-1][cb] == INF:
+        if 0 < emoji-1 and counter[emoji-1][cb] == 0:
             q.append((emoji-1, cb))
             counter[emoji-1][cb] = counter[emoji][cb] + 1
 
-    return min(counter[S])
+    # return min(counter[S])
+    res = int(1e9)
+    for c in counter[S]:
+        if c:
+            res = min(res, c)
 
+    return res
 
 print(bfs())
