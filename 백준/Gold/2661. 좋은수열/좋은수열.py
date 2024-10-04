3ae3e3d69b45f73c74e49910ad4f1820c251c60e
Jinyeong <139422554+jinddobaegi@users.noreply.github.com>
2024-10-04 15:32:47 +0900

    [Gold IV] Title: 좋은수열, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -4,10 +4,23 @@ input = stdin.readline
 
 N = int(input())
 
+# 길이가 N인 수열 만들 것
+# 동일한 부분수열이 인접해있으면 안됨
+# 이어 붙여 하나의 정수로 보았을 때 가능한 N자리 수 중 가장 작도록
+
+# 일단 각 숫자의 이전 자리를 파악해야
+# 같은 숫자가 나왔을 때 패턴을 찾을 수 있을 것 같음
+
+# 1) 재귀함수로 수열을 만들어 나가면서
+# 2) 패턴 체크
+# 3) N의 길이가 완성되면 컷
+
 
 def check_pattern(s_len, num_str):
-    for x in range(s_len-1):  # 절반 나눴을 때 앞 문자열 비교 시작점
+    for x in range(s_len-1):  # 절반 나눠서 앞 부분 비교 시작점
         for y in range(1, s_len//2+1):  # 비교할 패턴의 길이
+            if x+y+y > s_len:  # 뒷 부분 끝 점이 범위를 넘어가는지 확인
+                break
             if num_str[x:x+y] == num_str[x+y:x+y+y]:
                 return False
     return True
