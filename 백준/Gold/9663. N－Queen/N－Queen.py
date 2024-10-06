202c9d3a39a43a9256c2c31c8b837522205b118c
Jinyeong <139422554+jinddobaegi@users.noreply.github.com>
2024-10-07 01:32:15 +0900

    [Gold IV] Title: N-Queen, Time: 21880 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,26 +1,27 @@
-# N-Queen
-# 각 행과 열에는 하나의 퀸만 존재할 수 있다
+from sys import stdin
+
+input = stdin.readline
 
 N = int(input())
 
-def attack(a):
-    for i in range(a):
-        # 같은 열에 있거나 대각선에 있다면?
-        if arr[a] == arr[i] or abs(arr[a] - arr[i]) == abs(a - i):
-            return False
-    return True
 
-def n_queens(start):
-    global ans
-    if start == N:
-        ans += 1
+def solution(n):
+    global res
+    if n == N:
+        res += 1
+        return
+
     else:
         for i in range(N):
-            arr[start] = i # (start, i)
-            if attack(start): # 퀸을 놓을 수 있으면?
-                n_queens(start + 1)
+            if v1[i] == 0 and v2[n+i] == 0 and v3[n-i] == 0:
+                v1[i] = v2[n+i] = v3[n-i] = 1
+                solution(n+1)
+                v1[i] = v2[n+i] = v3[n-i] = 0
 
-arr = [0] * N
-ans = 0
-n_queens(0)
-print(ans)
+
+res = 0
+v1 = [0] * N
+v2 = [0] * (N*2)
+v3 = [0] * (N*2)
+solution(0)
+print(res)
