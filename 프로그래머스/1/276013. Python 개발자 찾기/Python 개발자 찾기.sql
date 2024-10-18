1362af3803f3d19d892cafe0cd7d2aa767d96dce
Jinyeong <139422554+jinddobaegi@users.noreply.github.com>
2024-10-18 10:51:36 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -2,5 +2,5 @@
 
 SELECT id, email, first_name, last_name
 FROM Developer_infos
-WHERE skill_1 = "Python" OR skill_2 = "Python" OR skill_3 = "Python"
+WHERE "Python" IN (skill_1, skill_2, skill_3)
 ORDER BY id;
